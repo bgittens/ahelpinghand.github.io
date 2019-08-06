ce1d3529bdb89f983581604764f0f463223c5840
jpurista <52415436+jpurista@users.noreply.github.com>
2019-08-06 14:40:35 -0400

    08/06/19 2:40 PM

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,15 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyAjOEPov0G8Tea7Mjm_tTVVbwO7A5VPeQU",
+	authDomain: "a-helping-hand-551ce.firebaseapp.com",
+	databaseURL: "https://a-helping-hand-551ce.firebaseio.com",
+	projectId: "a-helping-hand-551ce",
+	storageBucket: "a-helping-hand-551ce.appspot.com",
+	messagingSenderId: "22285102950",
+	appId: "1:22285102950:web:c08150dbdde3ccfd"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 var db = firebase.firestore;
 var x = "";
 var slideIndex = 0;
@@ -11,7 +23,7 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1} 
   x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  setTimeout(carousel, 4000); // Change image every 4 seconds
 }
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -42,20 +54,24 @@ function showSlides(n) {
 
 $("form").submit( function(e){
 	e.preventDefault();	
-	var submit = $(this);
-  	var formID = submit.attr("id");
-	x = formID;
+  	
+	var formID = $(this).attr("id");
+	var opt1 = document.getElementsByClassName("");
+	var opt2 = document.getElementsByClassName(""); 
+	var opt3 =  document.getElementsByClassName("");
 	
+	x = formID;
+	search();
 });
 
 
 function search() {
-	
+	console.log(db.collection(x));
 	//doc search element based on what search button was clicked in the nav bar
-	db.collection(x).get().then(function(doc){
+	//db.collection(x).get().then(function(doc){
 		
 		
-	});
+	//});
 	
 	//get that collection based on the above and goi through the option that they chose (ex. location, interest, length)
 	
