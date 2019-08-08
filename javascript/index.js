5d7148e730097efe16fa9fb0319b47cac1891c84
jpurista <52415436+jpurista@users.noreply.github.com>
2019-08-08 12:58:59 -0400

    08/08/19 12:58 PM Search Finally Semi-working

--- javascript/index.js
+++ javascript/index.js
@@ -53,128 +53,101 @@ $("form").submit( function(e){
   	
 	var formID = $(this).attr("id");
 
-	console.log(formID);
 	var opt1;
 	var opt2;
 	var opt3;
 	var doc;
 	var doc2;
 	var doc3;
-	var opp = db.collection(formID);
 
 	if(formID === "opp_info") {
 		opt1 = document.getElementById("v_search_city").value;
-		doc = opp.where("location", "==", opt1);
+		doc = db.collection(formID).where("location", "==", opt1);
+		search(doc);
 		
-		console.log(doc);
-	} 
-	
-	else if(formID === "internships_info") {
+	} else if(formID === "internships_info") {
 		opt1 = document.getElementById("i_search_state").value;
 		opt2 = document.getElementById("i_search_interest").value;
-		doc = opp.where("state", "==", opt1);
-		doc2 = opp.where("interest", "==", opt2);
+		doc = db.collection(formID).where("state", "==", opt1);
+		doc2 = db.collection(formID).where("interest", "==", opt2);
 		
-		console.log(doc, doc2);
+		search2(doc, doc2);
 		//getResults(doc, opt1, doc2, opt2);
-	} 
-	
-	else if(formID === "a_submit") {
+	} else if(formID === "a_submit") {
 		opt1 = document.getElementById("a_search_state").value;
 		opt2 = document.getElementById("a_search_interest").value;
 		opt3 = document.getElementById("a_search_length").value;
-		doc = opp.where("state", "==", opt1);
-		doc2 = opp.where("interest", "==", opt2);
-		doc3 = opp.where("length", "==", opt3);
+		doc = db.collection(formID).where("state", "==", opt1);
+		doc2 = db.collection(formID).where("interest", "==", opt2);
+		doc3 = db.collection(formID).where("length", "==", opt3);
 		
-		console.log(doc, doc2, doc3);
+		search3(doc, doc2, doc3);
 		//getResults(doc, opt1, doc2, opt2, doc3, opt3);
-	} 
-	
-	else if(formID === "sc_submit") {
+	} else if(formID === "sc_submit") {
 		opt1 = document.getElementById("sc_search_type").value;
 		opt2 = document.getElementById("sc_search_interest").value;
-		doc = opp.where("type", "==", opt1);
-		doc2 = opp.where("interest", "==", opt2);
+		doc = db.collection(formID).where("type", "==", opt1);
+		doc2 = db.collection(formID).where("interest", "==", opt2);
 		
-		console.log(doc, doc2);
+		search2(doc, doc2);
 		//getResults(doc, opt1, doc2, opt2);
-	} 
-	
-	else if(formID === "collegeClass_info") {
+	} else if(formID === "collegeClass_info") {
 		opt1 = document.getElementById("c_search_state").value;
 		opt2 = document.getElementById("c_search_interest").value;
-		doc = opp.where("state", "==", opt1);
-		doc2 = opp.where("interest", "==", opt2);
+		doc = db.collection(formID).where("state", "==", opt1);
+		doc2 = db.collection(formID).where("interest", "==", opt2);
 		
-		console.log(doc, doc2);
+		search2(doc, doc2);
 		//getResults(doc, opt1, doc2, opt2);
-	} 
-	
-	else if(formID ==="summerProgram_info") {
+	} else if(formID ==="summerProgram_info") {
 		opt1 = document.getElementById("su_search_state").value;
 		opt2 = document.getElementById("su_search_interest").value;
 		opt3 = document.getElementById("su_search_length").value;
-		doc = opp.where("state", "==", opt1);
-		doc2 = opp.where("interest", "==", opt2);
-		doc3 = opp.where("length", "==", opt3);
+		doc = db.collection(formID).where("state", "==", opt1);
+		doc2 = db.collection(formID).where("interest", "==", opt2);
+		doc3 = db.collection(formID).where("length", "==", opt3);
 		
-		console.log(doc, doc2, doc3);
+		search3(doc, doc2, doc3);
 		//getResults(doc, opt1, doc2, opt2, doc3, opt3);
 	}
 	
 });
 
-/*
-//var citiesRef = db.collection("cities");
+function search (doc){
+		doc.get().then(function(result){
+		 // returns all matching documents
+			result.forEach(function (doc) {
+			  console.log(doc.data());
+			});
+		}).catch(function (err){
+			console.log(err);
+		// error handling
+		});
+} 
 
-//function search(doc) {
+function search2(doc, doc2){
+		doc.get().then(function(result){
+		 // returns all matching documents
+			result.forEach(function (doc) {
+			  	console.log(doc.data());
+				console.log(doc2.data());
+			});
+		}).catch(function (err){
+			console.log(err);
+		// error handling
+		});
+} 
 
-	//doc search element based on what search button was clicked in the nav bar
-	
-
-		
-		//getResults(opt1, opt2, opt3);
-	
-	
-	//get that collection based on the above and go through the option that they chose (ex. location, interest, length)
-	
-	
-	//choose the ones that match with their search and put those items in a temporary list that resets on every search
-	//for (var h = 0; h < list.length; h++){
-		
-	//}
-	
-	//go through a for loop to display each item based on list[i] (index); this is done to put them into the corresponding spaces in the premade template for search results
-//}
-//make alert if not all inputs are put in, search for top input first then look for more specifications
-
-//function inputAll() {
-//}
-
-function getResults(doc, opt1, doc2, opt2, doc3, opt3) {
-	
-	if(doc2 === undefined && doc3 === undefined){
-		console.log(doc.data().name);
-		
-	} 
-	
-	else if(doc3 === undefined){
-		console.log(doc.data().opt1, doc2.data().opt2);
-		
-	} 
-	
-	else{
-		console.log(doc.data().opt1, doc2.data().opt2, doc3.data().opt3);
-	}
+function search3(doc, doc2, doc3){
+		doc.get().then(function(result){
+		 // returns all matching documents
+			result.forEach(function (doc) {
+			  	console.log(doc.data());
+				console.log(doc2.data());
+				console.log(doc3.data());
+			});
+		}).catch(function (err){
+			console.log(err);
+		// error handling
+		});
 }
-
-if(opt2 === undefined && opt3 === undefined){
-		console.log(formID, opt1);
-		//getResults(opt1);
-		
-	} else if(opt3 === undefined){
-		console.log(formID, opt1, opt2);
-		//getResults(opt1, opt2);
-	} else{
-	*/
