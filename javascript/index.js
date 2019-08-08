85ccaa9672b231198b69fb95d9e70a76f13c7dc6
Juan Pablo Urista <jurista@andrew.cmu.edu>
2019-08-07 21:14:34 -0400

    08/07/19 9:14 PM

--- javascript/index.js
+++ javascript/index.js
@@ -10,7 +10,7 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-var db = firebase.firestore;
+var db = firebase.firestore();
 var x = "";
 var slideIndex = 0;
 var list = [];
@@ -22,7 +22,9 @@ function carousel() {
     x[i].style.display = "none"; 
   }
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1} 
+  if (slideIndex > x.length) {
+	  slideIndex = 1;
+  } 
   x[slideIndex-1].style.display = "block"; 
   setTimeout(carousel, 4000); // Change image every 4 seconds
 }
@@ -40,8 +42,12 @@ function currentSlide(n) {
 function showSlides(n) {
   var slides = document.getElementsByClassName("tests");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+	  slideIndex = 1;
+  } 
+    if (n < 1) {
+		slideIndex = slides.length;
+	}
     for (var i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
     }
@@ -57,54 +63,78 @@ $("form").submit( function(e){
 	e.preventDefault();	
   	
 	var formID = $(this).attr("id");
-	x = formID;
+
+	console.log(formID);
 	var opt1;
 	var opt2;
 	var opt3;
+	var doc;
+	var doc2;
+	var doc3;
 	
+
 	if(formID === "opp_info") {
-		opt1 = document.getElementById("v_search_city").value;
-	} else if(formID === "internships_info") {
+		doc = db.collection("opp_info").where("location", "==", "Dallas, TX");
+		console.log(doc);
+	} 
+	
+	else if(formID === "internships_info") {
 		opt1 = document.getElementById("i_search_city").value;
 		opt2 = document.getElementById("i_search_interest").value;
-	} else if(formID === "a_submit") {
-		opt1 = document.getElementById("a_search_city").value;
+		doc = db.collection("internships_info").where("state", "==", opt1);
+		doc2 = db.collection("internships_info").where("interest", "==", opt2);
+		console.log(doc, doc2);
+		//getResults(doc, opt1, doc2, opt2);
+	} 
+	
+	else if(formID === "a_submit") {
+		opt1 = document.getElementById("a_search_state").value;
 		opt2 = document.getElementById("a_search_interest").value;
 		opt3 = document.getElementById("a_search_length").value;
-	} else if(formID === "sc_submit") {
+		doc = db.collection("a_submit").where("state", "==", opt1);
+		doc2 = db.collection("a_submit").where("interest", "==", opt2);
+		doc3 = db.collection("a_submit").where("length", "==", opt3);
+		//getResults(doc, opt1, doc2, opt2, doc3, opt3);
+	} 
+	
+	else if(formID === "sc_submit") {
 		opt1 = document.getElementById("sc_search_type").value;
 		opt2 = document.getElementById("sc_search_interest").value;
-	} else if(formID === "collegeClass_info") {
-		opt1 = document.getElementById("c_search_city").value;
+		doc = db.collection(formID).where("type", "==", opt1);
+		doc2 = db.collection(formID).where("interest", "==", opt2);
+		//getResults(doc, opt1, doc2, opt2);
+	} 
+	
+	else if(formID === "collegeClass_info") {
+		opt1 = document.getElementById("c_search_state").value;
 		opt2 = document.getElementById("c_search_interest").value;
-	} else if(formID ==="summerProgram_info") {
+		doc = db.collection(formID).where("state", "==", opt1);
+		doc2 = db.collection(formID).where("interest", "==", opt2);
+		//getResults(doc, opt1, doc2, opt2);
+	} 
+	
+	else if(formID ==="summerProgram_info") {
 		opt1 = document.getElementById("su_search_state").value;
 		opt2 = document.getElementById("su_search_interest").value;
 		opt3 = document.getElementById("su_search_length").value;
+		doc = db.collection(formID).where("state", "==", opt1);
+		doc2 = db.collection(formID).where("interest", "==", opt2);
+		doc3 = db.collection(formID).where("length", "==", opt3);
+		//getResults(doc, opt1, doc2, opt2, doc3, opt3);
 	}
-	
-	search(formID, opt1, opt2, opt3);
 	
 });
 
 //var citiesRef = db.collection("cities");
 
-function search(formID, opt1, opt2, opt3) {
+//function search(doc) {
 
 	//doc search element based on what search button was clicked in the nav bar
 	
-	if(opt2 === undefined && opt3 === undefined){
-		console.log(formID, opt1);
-		getResults();
+
 		
-	} else if(opt3 === undefined){
-		console.log(formID, opt1, opt2);
-
-	} else{
-		console.log(formID, opt1, opt2, opt3);
-
-	}
-
+		//getResults(opt1, opt2, opt3);
+	
 	
 	//get that collection based on the above and go through the option that they chose (ex. location, interest, length)
 	
@@ -115,16 +145,35 @@ function search(formID, opt1, opt2, opt3) {
 	//}
 	
 	//go through a for loop to display each item based on list[i] (index); this is done to put them into the corresponding spaces in the premade template for search results
-}
+//}
 //make alert if not all inputs are put in, search for top input first then look for more specifications
 
-function inputAll() {
+//function inputAll() {
+//}
+
+function getResults(doc, opt1, doc2, opt2, doc3, opt3) {
 	
-}
-function getResults() {
+	if(doc2 === undefined && doc3 === undefined){
+		console.log(doc.data().name);
+		
+	} 
 	
-	for (var j = 0; j < list.length; j++){
-		var click = document.getElementById(x.value([i]));
-		list[i] = click;
+	else if(doc3 === undefined){
+		console.log(doc.data().opt1, doc2.data().opt2);
+		
+	} 
+	
+	else{
+		console.log(doc.data().opt1, doc2.data().opt2, doc3.data().opt3);
 	}
 }
+
+/*	if(opt2 === undefined && opt3 === undefined){
+		console.log(formID, opt1);
+		//getResults(opt1);
+		
+	} else if(opt3 === undefined){
+		console.log(formID, opt1, opt2);
+		//getResults(opt1, opt2);
+	} else{
+	*/
